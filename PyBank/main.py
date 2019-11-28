d0bd7af34471e57c7b98e2d52daa8cc31ac6702e
Alyssa <allym.moyer21@gmail.com>
2019-11-27 21:28:36 -0800

    Adding finished Pybank code and the results

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,7 +5,6 @@ csvpath = os.path.join('..', 'budget_data.csv')
 
 budget_dict={}
 change_dict = {}
-change_list = ()
 
 with open(csvpath, newline='') as csvfile:
 
@@ -31,12 +30,35 @@ num_of_months = len(change_dict)
 #take out first value of change_dict because not a real change calculation since first value of the series    
 change_dict.pop("Jan-10")
 
+#lists of values and keys
+change_listv = list(change_dict.values())
+change_listk = list(change_dict.keys())
 
+#finds average change in profit/loss by dividing the sum of the list of change by the length
+average_change = round((sum(change_listv)/len(change_listv)),2)
 
+#calculates max and min values of the change list(made of values of the change dictonary)
+#then finds the matching month(by index) in the change list(made of keys)
+greatest_increase = max(change_listv)
+greatest_increase_mon = change_listk[change_listv.index(max(change_listv))]
 
-change_list = list(change_dict.values())
-average_change = sum(change_list)/len(change_list)
-print(average_change)
+greatest_decrease = min(change_listv)
+greatest_decrease_mon= change_listk[change_listv.index(min(change_listv))]
 
+#print out results
+print(
+    f"""Total Months: {num_of_months}
+Total ${net_total}
+Average Change ${average_change}
+Greatest Increase in Profits: {greatest_increase_mon} (${greatest_increase})
+Greatest Decrease in Profits: {greatest_decrease_mon} (${greatest_decrease})"""
+    )
 
-
+#write results to a text file
+results_file = open("results.txt","w") 
+results_file.write( f"""Total Months: {num_of_months}
+Total ${net_total}
+Average Change ${average_change}
+Greatest Increase in Profits: {greatest_increase_mon} (${greatest_increase})
+Greatest Decrease in Profits: {greatest_decrease_mon} (${greatest_decrease})"""
+    )
